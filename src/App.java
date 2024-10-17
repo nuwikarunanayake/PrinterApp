c2a1ad93e60aab7f2cd8dfd4cfe777259a968d95
nuwikarunanayake <nuwangiprabhara@gmail.com>
2024-10-17 21:49:25 +0530

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,57 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String text = "Printer App";
+        String text1 = "This App is made by:";
+        String creator = "Nuwangi"; // Q4 (Assign my name as the value of the creator variable.)
+
+        //Q2.Print “Printer App” to the console.
+        System.out.println(text); 
+
+        //Q3.Print “This app is made by:” to the console.
+        System.out.println(text1); 
+
+        //Q5.Print the value of the creator to the console. 
+       System.out.print("This App is made by: ");
+       System.out.println(creator);
+
+       // Q6.Declare two numeric variables 
+       int number1 = 5;
+       int number2 = 2;
+
+       System.out.println( number1); //Q7
+
+       System.out.println("The value of number1 is" + number1); // Q8
+       System.out.println("The value of number2 is " + number1); //Q9
+
+        int multiplication; // Q10
+        multiplication = number1 * number2 ; // Q11 : For multiplication
+        System.out.println(number1 + " * " + number2 + " = " + multiplication); // Q12
+         
+        System.out.println(multiplication); 
+
+        // for addition,
+        int addition = number1 + number2;
+        System.out.println(number1 + "+" + number2 + "=" + addition);
+        System.out.println(addition);
+
+
+        double division = number1 / number2;
+        System.out.println(number1 + "/" + number2 + "=" + division);
+        System.out.println(division); 
+
+
+        int substraction = number1 - number2;
+        System.out.println(number1 + "-" + number2 + "=" + substraction);
+        System.out.println(substraction); 
+
+
+
+
+
+
+
+
+
+
+    }
+}
