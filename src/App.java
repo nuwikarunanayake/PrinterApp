02936ae6c9a7fb582a0eecac5302907fe31f6999
nuwikarunanayake <nuwangiprabhara@gmail.com>
2024-10-17 22:12:08 +0530

    Move all the declarations of the variables to the top. and corrected the variable of the line 31 as number2.

--- src/App.java
+++ src/App.java
@@ -4,6 +4,16 @@ public class App {
         String text1 = "This App is made by:";
         String creator = "Nuwangi"; // Q4 (Assign my name as the value of the creator variable.)
 
+       // Q6.Declare two numeric variables, 
+       int number1 = 5;
+       int number2 = 2;
+
+       int multiplication; // Q10
+       multiplication = number1 * number2 ; 
+       int addition = number1 + number2;
+       double division = number1 / number2;
+       int substraction = number1 - number2;
+
         //Q2.Print “Printer App” to the console.
         System.out.println(text); 
 
@@ -14,33 +24,29 @@ public class App {
        System.out.print("This App is made by: ");
        System.out.println(creator);
 
-       // Q6.Declare two numeric variables, 
-       int number1 = 5;
-       int number2 = 2;
-
+     
        // System.out.println( number1); //Q7
 
        System.out.println("The value of number1 is " + number1); // Q8
-       System.out.println("The value of number2 is " + number1); //Q9
+       System.out.println("The value of number2 is " + number2); //Q9
 
-        int multiplication; // Q10
-        multiplication = number1 * number2 ; // Q11 : For multiplication
+        
+        // Q11 : For multiplication
         System.out.println(number1 + " * " + number2 + " = " + multiplication); // Q12
-         
         System.out.println(multiplication); 
 
         // for addition,
-        int addition = number1 + number2;
+       
         System.out.println(number1 + " + " + number2 + " = " + addition);
         System.out.println(addition);
 
         // for division,
-        double division = number1 / number2;
+        
         System.out.println(number1 + " / " + number2 + " = " + division);
         System.out.println(division); 
 
         // for substraction,
-        int substraction = number1 - number2;
+       
         System.out.println(number1 + " - " + number2 + " = " + substraction);
         System.out.println(substraction); 
 
